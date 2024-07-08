9bf0c2bd332154082bc88148ff5f6bce43babce8
saransh <sethia.saransh.ss@gmail.com>
2024-07-08 19:09:52 +0530

    fifth commit

--- frontend/github/src/pages/ExplorePage.js
+++ frontend/github/src/pages/ExplorePage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import javascript from '../assets/javascript.svg';
+import typescript from '../assets/typescript.svg';
+import c from '../assets/c.svg';
+import python from '../assets/python.svg';
+import java from '../assets/java.svg';
 
 const ExplorePage = () => {
-  return (
-    <div>
-      ExplorePage
-    </div>
-  )
-}
+	return (
+		<div className='px-4'>
+			<div className='bg-glass max-w-2xl mx-auto rounded-md p-4'>
+				<h1 className='text-xl font-bold text-center'>Explore Popular Repositories</h1>
+				<div className='flex flex-wrap gap-2 my-2 justify-center'>
+					<img src={javascript} alt='JavaScript' className='h-11 sm:h-20 cursor-pointer' />
+					<img src={typescript} alt='TypeScript logo' className='h-11 sm:h-20 cursor-pointer' />
+					<img src={c} alt='C++ logo' className='h-11 sm:h-20 cursor-pointer' />
+					<img src={python} alt='Python logo' className='h-11 sm:h-20 cursor-pointer' />
+					<img src={java} alt='Java logo' className='h-11 sm:h-20 cursor-pointer' />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ExplorePage
